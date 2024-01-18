21d141770638711105f5c80f744cc4fd906edb65
Samuel Jeffman <sjeffman83@outlook.com>
2024-01-18 17:26:26 +0100

    Added classes

--- PetProject/PetProject/obj/Debug/net8.0/PetProject.AssemblyInfo.cs
+++ PetProject/PetProject/obj/Debug/net8.0/PetProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dc42c9fa408340e59dd6da7ab6261298eedbc46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+212025b82f560b601076473db9ec7d829f23ca09")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
