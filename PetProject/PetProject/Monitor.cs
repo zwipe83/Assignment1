db47a2b968b8e027295f4423e735469f90284b1a
Samuel Jeffman <sjeffman83@outlook.com>
2024-01-21 13:44:12 +0100

    Added functionality to class

--- PetProject/PetProject/Monitor.cs
+++ PetProject/PetProject/Monitor.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace PetProject
 {
@@ -14,34 +15,80 @@ namespace PetProject
         private int horizontalResolutionInPixels;
         private int verticalResolutionInPixels;
         private int totalResolutionInPixels;
-        private Color color;
+        private double price;
+        private DateTime purchaseDate;
 
         private void SaveManufacturer()
         {
-            manufacturer = "";
+            Console.WriteLine("\nWhat is the name of the manufacturer of your monitor?");
+            manufacturer = Console.ReadLine();
         }
-        private void SaveModel() 
+        private void SaveModel()
         {
-            modelName = "";
+            Console.WriteLine("\nWhat is the model name?");
+            modelName = Console.ReadLine();
         }
 
         private void SaveHorizontalResolution()
         {
-            horizontalResolutionInPixels = 0;
+            Console.WriteLine("\nWhat is the horizontal resolution in pixels?");
+            if (!int.TryParse(Console.ReadLine(), out horizontalResolutionInPixels))
+            {
+                horizontalResolutionInPixels = 1920;
+            }
         }
         private void SaveVerticalResolution()
         {
-            verticalResolutionInPixels = 0;
+            Console.WriteLine("\nWhat is the vertical resolution in pixels?");
+            if (!int.TryParse(Console.ReadLine(), out verticalResolutionInPixels))
+            {
+                verticalResolutionInPixels = 1080;
+            }
         }
 
-        private void totalResolution()
+        private void CalculateTotalResolution()
         {
             totalResolutionInPixels = horizontalResolutionInPixels * verticalResolutionInPixels;
         }
 
-        private void SaveColor()
+        private void SavePurchaseDate()
         {
-            color = Color.Black;
+            Console.WriteLine("\nWhat date did you buy your monitor?");
+            if (!DateTime.TryParse(Console.ReadLine(), out purchaseDate))
+            {
+                purchaseDate = DateTime.Today;
+            }
+        }
+        private void SavePrice()
+        {
+            Console.WriteLine("\nWhat date did it cost?");
+            if(!double.TryParse(Console.ReadLine(), out price))
+            {
+                price = 0.0;
+            }
+        }
+
+        private void SaveData()
+        {
+            SaveManufacturer();
+            SaveModel();
+            SaveHorizontalResolution();
+            SaveVerticalResolution();
+            CalculateTotalResolution();
+            SavePurchaseDate();
+            SavePrice();
+        }
+
+        private void DisplayInfo()
+        {
+            Console.WriteLine($"{manufacturer} - {modelName} - {horizontalResolutionInPixels} - {verticalResolutionInPixels} - {totalResolutionInPixels} - {purchaseDate.Date.ToString("yyyy-MM-dd")} - {price}");
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("\n------------------------\nWelcome to the electronics store!");
+            SaveData();
+            DisplayInfo();
         }
     }
 }
